7a186a585e76b80f21ff11d90b4404dbc6a03615
Sparko-Sol <accounts@sparkosol.com>
2023-10-27 18:14:04 +0500

    url changes

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -45,7 +45,7 @@ const LoginForm = () => {
   useEffect(() => {
     const token = localStorage.getItem('cw-access-token');
     if(currentUser && token){
-      currentUser.workspaces.length === 1 ? window.location.href = `http://${currentUser.workspaces[0].domain}.localhost:5173/dashboard?token=${token}` : navigate('/domain/select')
+      currentUser.workspaces.length === 1 ? window.location.href = `http://${currentUser.workspaces[0].domain}.techtrival.com/dashboard?token=${token}` : navigate('/domain/select')
     }
   }, [currentUser]);
 
