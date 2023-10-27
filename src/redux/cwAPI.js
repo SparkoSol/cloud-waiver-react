b035defb6cf6ac0614f04c1adeabf40cfb53242a
Sparko-Sol <accounts@sparkosol.com>
2023-10-27 18:05:14 +0500

    url chnaged

--- src/redux/cwAPI.js
+++ src/redux/cwAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const baseUrl = 'https://cloudwaiver.sparkosol.com'
-const baseUrl = 'http://192.168.1.36:3000'
+const baseUrl = 'https://cloudwaiver.sparkosol.com'
+// const baseUrl = 'http://192.168.1.36:3000'
 const cwAPI = axios
 
 // Add a request interceptor
