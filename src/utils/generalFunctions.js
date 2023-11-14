8a64e2cd3ba64f8425a8ca57c3ae108037416cf5
Sparko-Sol <accounts@sparkosol.com>
2023-11-14 15:11:13 +0500

    static button

--- src/utils/generalFunctions.js
+++ src/utils/generalFunctions.js
@@ -538,6 +538,7 @@ const templates = {
         });
         let openButton = $('<button>', {
           text: 'Upload Files',
+          type: 'button',
           class: 'px-3 py-2 text-white bg-btnBg rounded-lg',
           click: function () {
             $(`#fileInput-${fieldData.name}`).click();
