2f6b3be8a788c9ca21e6047820c9f36bf6a180a9
Qulbe12 <qulbehussain@outlook.com>
2023-12-05 15:42:20 +0500

    changes the api link for the pdf route

--- src/redux/cwAPI.js
+++ src/redux/cwAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://api.cloudwaiver.com'
+const baseUrl = 'https://api.cloudwaiver.com'
 // const baseUrl = 'http://192.168.1.36:3000'
 const cwAPI = axios
 
