125788a4c0e44f9eb50b594523c4622b4b4e3f57
Qulbe12 <qulbehussain@outlook.com>
2023-12-05 15:17:40 +0500

    changes the api link for the pdf route

--- src/redux/cwAPI.js
+++ src/redux/cwAPI.js
@@ -1,52 +1,52 @@
 import axios from 'axios'
 
-const baseUrl = 'https://cloudwaiver.sparkosol.com'
+const baseUrl = 'http://api.cloudwaiver.com'
 // const baseUrl = 'http://192.168.1.36:3000'
 const cwAPI = axios
 
 // Add a request interceptor
 axios.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('cw-access-token')
-      config.headers['Authorization'] = 'Bearer ' + token
-      config.headers['X-TENANT-ID'] = getDynamicTenantId();
-    return config
-  },
-  error => {
-    Promise.reject(error)
-  }
+    config => {
+        const token = localStorage.getItem('cw-access-token')
+        config.headers['Authorization'] = 'Bearer ' + token
+        config.headers['X-TENANT-ID'] = getDynamicTenantId();
+        return config
+    },
+    error => {
+        Promise.reject(error)
+    }
 )
 
 window.http = cwAPI
 
 export const postRequest = (url, body) => {
-  return cwAPI.post(`${baseUrl}${url}`, body)
+    return cwAPI.post(`${baseUrl}${url}`, body)
 }
 
 export const getRequest = (url) => {
-  return cwAPI.get(`${baseUrl}${url}`)
+    return cwAPI.get(`${baseUrl}${url}`)
 }
 
 export const putRequest = (url, body) => {
-  return cwAPI.put(`${baseUrl}${url}`, body)
+    return cwAPI.put(`${baseUrl}${url}`, body)
 }
 
 export const deleteRequest = (url, body) => {
-  return cwAPI.delete(`${baseUrl}${url}`, body)
+    return cwAPI.delete(`${baseUrl}${url}`, body)
 }
 
 export const patchRequest = (url, body) => {
-  return cwAPI.patch(`${baseUrl}${url}`, body)
+    return cwAPI.patch(`${baseUrl}${url}`, body)
 }
 
 //extract X_tan id
 export function getDynamicTenantId() {
-  const currentURL = window.location.href;
-  const urlParts = currentURL.split('.');
-  if (urlParts[0].includes('https')) {
-    return urlParts[0].replace('https://', '');
-  }
-  return urlParts[0].replace('http://', '');
+    const currentURL = window.location.href;
+    const urlParts = currentURL.split('.');
+    if (urlParts[0].includes('https')) {
+        return urlParts[0].replace('https://', '');
+    }
+    return urlParts[0].replace('http://', '');
 }
 
 export default cwAPI
