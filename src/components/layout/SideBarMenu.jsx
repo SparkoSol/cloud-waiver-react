969de1508666590db6f76e8e5837becfa81ac8f0
Qulbe12 <qulbehussain@outlook.com>
2023-12-05 15:47:27 +0500

    techtrivel domain name changed to cloudwaiver

--- src/components/layout/SideBarMenu.jsx
+++ src/components/layout/SideBarMenu.jsx
@@ -9,143 +9,143 @@ import {useRef} from "react";
 import {patchRequest, postRequest} from "../../redux/cwAPI";
 
 const SideBarMenu = ({
-                       searchRef,
-                       data,
-                       open,
-                       openReplyMenuIndex,
-                       handleReplyClick,
-                       hover,
-                       setHover,
-                       width,
-                       setOpen
+                         searchRef,
+                         data,
+                         open,
+                         openReplyMenuIndex,
+                         handleReplyClick,
+                         hover,
+                         setHover,
+                         width,
+                         setOpen
                      }) => {
-  const {pathname} = useLocation();
-  const dispatch = useDispatch();
-  const currentUser = useSelector(selectCurrentUser);
-  const imageRef = useRef();
+    const {pathname} = useLocation();
+    const dispatch = useDispatch();
+    const currentUser = useSelector(selectCurrentUser);
+    const imageRef = useRef();
 
-  const handleFileChange = async (e) => {
-    const selectedFile = e.target.files[0];
-    const frmData = new FormData();
-    frmData.append('file', selectedFile);
-    const {data} = await postRequest('/upload', frmData)
-    dispatch(updateUserProfile(data.url))
-    await patchRequest(`/persons/${currentUser._id}`, {
-      profile_picture: data.url
-    })
-  };
-  return (
-    <aside
-      onMouseEnter={() => {
-        if (width > 1024) {
-          setHover(true);
-        }
-      }}
-      onMouseLeave={() => {
-        if (width > 1024) {
-          setHover(false);
-        }
-      }}
-      className={`${
-        open || hover
-          ? 'w-64 p-3'
-          : width > 1024
-            ? 'w-20 p-3'
-            : 'w-0'
-      } bg-bgDark h-screen transition-all duration-500 font-mulish fixed top-0 left-0 overflow-y-scroll scrollbar-thin z-20`}>
-      <div className="w-full border-b border-btnBg py-4">
-        <Link className={`transition-all duration-500 ${open || hover ? 'w-28' : 'w-8'}`} to={'/dashboard'}>
-          <img className='w-full max-w-[112px] mx-auto' alt='Loading..' src='/images/logo.png'/>
-        </Link>
-      </div>
-      <div className="flex gap-2 items-center py-4 w-60 px-2 border-b border-btnBg">
-        <div className="w-8 h-8 rounded-full border border-1 border-iconGray bg-white overflow-hidden">
-          <img
-            src={currentUser?.profile_picture || '/images/user.png'}
-            className='w-full h-full'
-            alt=""
-            ref={imageRef}
-            onClick={e => imageRef.current.click()}
-          />
-          <input
-            type='file'
-            className='hidden'
-            onChange={handleFileChange}
-            ref={imageRef}
-          />
-        </div>
-        {(open || hover) && <Link className="text-sm text-iconGray font-semibold" to="/dashboard">
-          {`${currentUser?.first_name} ${currentUser?.last_name}`}
-        </Link>}
-      </div>
-      <div className='block lg:hidden'>
-        <Input extraClasses='pt-4' inputRef={searchRef} BtnIcon={MagnifyingGlassIcon} placeholder='Search'/>
-      </div>
-      <ul>
-        {data.map((item, index) => (
-          <li className='text-sm relative font-semibold text-iconGray w-[244px]' key={index}>
-            {item.subList ? (
-              <div>
-                <button onClick={() => handleReplyClick(index)}
-                        className={`flex items-center gap-4 py-2.5 pl-2 mt-2.5 w-full relative ${pathname.includes(item.url) ? 'text-blue-400' : 'text-iconGray'}`}>
-                  <item.icon className='w-7 h-7'/>
-                  <span
-                    className={`${open || hover ? 'opacity-100' : 'opacity-0'} transition-all duration-500`}>
+    const handleFileChange = async (e) => {
+        const selectedFile = e.target.files[0];
+        const frmData = new FormData();
+        frmData.append('file', selectedFile);
+        const {data} = await postRequest('/upload', frmData)
+        dispatch(updateUserProfile(data.url))
+        await patchRequest(`/persons/${currentUser._id}`, {
+            profile_picture: data.url
+        })
+    };
+    return (
+        <aside
+            onMouseEnter={() => {
+                if (width > 1024) {
+                    setHover(true);
+                }
+            }}
+            onMouseLeave={() => {
+                if (width > 1024) {
+                    setHover(false);
+                }
+            }}
+            className={`${
+                open || hover
+                    ? 'w-64 p-3'
+                    : width > 1024
+                        ? 'w-20 p-3'
+                        : 'w-0'
+            } bg-bgDark h-screen transition-all duration-500 font-mulish fixed top-0 left-0 overflow-y-scroll scrollbar-thin z-20`}>
+            <div className="w-full border-b border-btnBg py-4">
+                <Link className={`transition-all duration-500 ${open || hover ? 'w-28' : 'w-8'}`} to={'/dashboard'}>
+                    <img className='w-full max-w-[112px] mx-auto' alt='Loading..' src='/images/logo.png'/>
+                </Link>
+            </div>
+            <div className="flex gap-2 items-center py-4 w-60 px-2 border-b border-btnBg">
+                <div className="w-8 h-8 rounded-full border border-1 border-iconGray bg-white overflow-hidden">
+                    <img
+                        src={currentUser?.profile_picture || '/images/user.png'}
+                        className='w-full h-full'
+                        alt=""
+                        ref={imageRef}
+                        onClick={e => imageRef.current.click()}
+                    />
+                    <input
+                        type='file'
+                        className='hidden'
+                        onChange={handleFileChange}
+                        ref={imageRef}
+                    />
+                </div>
+                {(open || hover) && <Link className="text-sm text-iconGray font-semibold" to="/dashboard">
+                    {`${currentUser?.first_name} ${currentUser?.last_name}`}
+                </Link>}
+            </div>
+            <div className='block lg:hidden'>
+                <Input extraClasses='pt-4' inputRef={searchRef} BtnIcon={MagnifyingGlassIcon} placeholder='Search'/>
+            </div>
+            <ul>
+                {data.map((item, index) => (
+                    <li className='text-sm relative font-semibold text-iconGray w-[244px]' key={index}>
+                        {item.subList ? (
+                            <div>
+                                <button onClick={() => handleReplyClick(index)}
+                                        className={`flex items-center gap-4 py-2.5 pl-2 mt-2.5 w-full relative ${pathname.includes(item.url) ? 'text-blue-400' : 'text-iconGray'}`}>
+                                    <item.icon className='w-7 h-7'/>
+                                    <span
+                                        className={`${open || hover ? 'opacity-100' : 'opacity-0'} transition-all duration-500`}>
                       <p className='whitespace-nowrap'>{item.title}</p><ChevronDownIcon
-                    className='w-3 h-3 absolute right-4 top-1/2 transform -translate-y-1/2'/>
+                                        className='w-3 h-3 absolute right-4 top-1/2 transform -translate-y-1/2'/>
                     </span>
 
-                </button>
-                <Transition
-                  show={openReplyMenuIndex === index}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                  enterTo="opacity-100 translate-y-0 sm:scale-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                >
-                  <ul className="w-full">
-                    {item.subList.map((subItem, subIndex) => {
-                      return (
-                        <li key={subIndex}>
-                          <Link to={subItem.url} onClick={e => {
-                            if (width < 1024) {
-                              setOpen(false);
-                            }
-                          }}
-                                className={`flex items-center gap-6 p-2.5 ${pathname.includes(subItem.title.toLowerCase()) ? 'text-blue-400' : 'text-iconGray'}`}>
-                            <span className="w-7 text-center">{subItem.title[0]}</span>
-                            {subItem.title}
-                          </Link>
-                        </li>)
-                    })}
-                  </ul>
-                </Transition>
-              </div>
-            ) : (
-              <Link to={item.url}
-                    onClick={item.url === "#" ? () => {
-                        localStorage.clear();
-                        window.location.href = 'https://techtrival.com';
-                      } :
-                      () => {
-                        if (width < 1024) {
-                          setOpen(false);
-                        }
-                      }
-                    }
-                    className={`flex items-center gap-4 py-2.5 pl-2 mt-2.5 ${pathname.includes(item.url) ? 'text-blue-400' : 'text-iconGray'}`}>
-                <item.icon className='w-7 h-7'/>
-                <p
-                  className={`${open || hover ? 'opacity-100' : 'opacity-0'} transition-all duration-500 whitespace-nowrap`}>{item.title}</p>
-              </Link>
-            )}
-          </li>
-        ))}
-      </ul>
-    </aside>
-  )
+                                </button>
+                                <Transition
+                                    show={openReplyMenuIndex === index}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                >
+                                    <ul className="w-full">
+                                        {item.subList.map((subItem, subIndex) => {
+                                            return (
+                                                <li key={subIndex}>
+                                                    <Link to={subItem.url} onClick={e => {
+                                                        if (width < 1024) {
+                                                            setOpen(false);
+                                                        }
+                                                    }}
+                                                          className={`flex items-center gap-6 p-2.5 ${pathname.includes(subItem.title.toLowerCase()) ? 'text-blue-400' : 'text-iconGray'}`}>
+                                                        <span className="w-7 text-center">{subItem.title[0]}</span>
+                                                        {subItem.title}
+                                                    </Link>
+                                                </li>)
+                                        })}
+                                    </ul>
+                                </Transition>
+                            </div>
+                        ) : (
+                            <Link to={item.url}
+                                  onClick={item.url === "#" ? () => {
+                                          localStorage.clear();
+                                          window.location.href = 'https://cloudwaiver.com';
+                                      } :
+                                      () => {
+                                          if (width < 1024) {
+                                              setOpen(false);
+                                          }
+                                      }
+                                  }
+                                  className={`flex items-center gap-4 py-2.5 pl-2 mt-2.5 ${pathname.includes(item.url) ? 'text-blue-400' : 'text-iconGray'}`}>
+                                <item.icon className='w-7 h-7'/>
+                                <p
+                                    className={`${open || hover ? 'opacity-100' : 'opacity-0'} transition-all duration-500 whitespace-nowrap`}>{item.title}</p>
+                            </Link>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    )
 }
 
 export default SideBarMenu;
--- src/pages/template/components/TemplateContainer.jsx
+++ src/pages/template/components/TemplateContainer.jsx
@@ -13,50 +13,52 @@ import toast from 'react-hot-toast';
 import {selectCurrentUser} from "../../../redux/user/userSlice";
 
 const TemplateContainer = ({children}) => {
-  const dispatch = useDispatch();
-  const waiver = useSelector(selectSingleWaiver);
-  const currentUser = useSelector(selectCurrentUser);
-  const [editMode, setEditMode] = useState(false)
-  const [loading, setLoading] = useState(false);
-  const {id} = useParams();
-  useEffect(() => {
-    setLoading(true);
-    dispatch(getSingleWaiver(id))
-      .finally(() => setLoading(false))
-    // eslint-disable-next-line
-  }, []);
-
-  function handleEdit(name) {
-    setLoading(true);
-    patchRequest(`/waivers/${id}`, {name})
-      .then(r => {
-        toast.success('Updated Successfully');
+    const dispatch = useDispatch();
+    const waiver = useSelector(selectSingleWaiver);
+    const currentUser = useSelector(selectCurrentUser);
+    const [editMode, setEditMode] = useState(false)
+    const [loading, setLoading] = useState(false);
+    const {id} = useParams();
+    useEffect(() => {
+        setLoading(true);
         dispatch(getSingleWaiver(id))
-          .finally(() => setLoading(false))
-      })
-      .catch(e => e.response.data.message)
-      .finally(() => setLoading(false))
-  }
+            .finally(() => setLoading(false))
+        // eslint-disable-next-line
+    }, []);
 
-  return (
-    <main>
-      {loading && <Spinner/>}
-      <div className='px-2 py-3'>
-        <div className="flex items-center space-x-4">
-          <h2 className="font-bold text-2xl">{waiver?.name}</h2>
-          <button className='outline-none' onClick={e => setEditMode(true)}>
-            <PencilIcon className='w-5 h-5'/>
-          </button>
-          <Modal setOpen={setEditMode} open={editMode} editMode={true} title='Edit Template' functionCall={handleEdit} value={waiver?.name}/>
-        </div>
-        <span className="text-sm italic">{`${currentUser?.domain}.techtrival.com/template/${waiver?._id}`}</span>
-      </div>
-      <Tabs tabs={tabsData}/>
-      <div className='shadow rounded-md sm:overflow-hidden bg-white py-6 px-4 space-y-6 sm:p-6'>
-        {children}
-      </div>
-    </main>
-  )
+    function handleEdit(name) {
+        setLoading(true);
+        patchRequest(`/waivers/${id}`, {name})
+            .then(r => {
+                toast.success('Updated Successfully');
+                dispatch(getSingleWaiver(id))
+                    .finally(() => setLoading(false))
+            })
+            .catch(e => e.response.data.message)
+            .finally(() => setLoading(false))
+    }
+
+    return (
+        <main>
+            {loading && <Spinner/>}
+            <div className='px-2 py-3'>
+                <div className="flex items-center space-x-4">
+                    <h2 className="font-bold text-2xl">{waiver?.name}</h2>
+                    <button className='outline-none' onClick={e => setEditMode(true)}>
+                        <PencilIcon className='w-5 h-5'/>
+                    </button>
+                    <Modal setOpen={setEditMode} open={editMode} editMode={true} title='Edit Template'
+                           functionCall={handleEdit} value={waiver?.name}/>
+                </div>
+                <span
+                    className="text-sm italic">{`${currentUser?.domain}.cloudwaiver.com/template/${waiver?._id}`}</span>
+            </div>
+            <Tabs tabs={tabsData}/>
+            <div className='shadow rounded-md sm:overflow-hidden bg-white py-6 px-4 space-y-6 sm:p-6'>
+                {children}
+            </div>
+        </main>
+    )
 }
 
 export default TemplateContainer
