ba48d236678d146ff9750408d9eb4d49ef760470
Sparko-Sol <accounts@sparkosol.com>
2023-12-09 17:36:20 +0500

    Ref No

--- src/redux/cwAPI.js
+++ src/redux/cwAPI.js
@@ -6,47 +6,47 @@ const cwAPI = axios
 
 // Add a request interceptor
 axios.interceptors.request.use(
-    config => {
-        const token = localStorage.getItem('cw-access-token')
-        config.headers['Authorization'] = 'Bearer ' + token
-        config.headers['X-TENANT-ID'] = getDynamicTenantId();
-        return config
-    },
-    error => {
-        Promise.reject(error)
-    }
+  config => {
+    const token = localStorage.getItem('cw-access-token')
+    config.headers['Authorization'] = 'Bearer ' + token
+    config.headers['X-TENANT-ID'] = getDynamicTenantId();
+    return config
+  },
+  error => {
+    Promise.reject(error)
+  }
 )
 
 window.http = cwAPI
 
 export const postRequest = (url, body) => {
-    return cwAPI.post(`${baseUrl}${url}`, body)
+  return cwAPI.post(`${baseUrl}${url}`, body)
 }
 
 export const getRequest = (url) => {
-    return cwAPI.get(`${baseUrl}${url}`)
+  return cwAPI.get(`${baseUrl}${url}`)
 }
 
 export const putRequest = (url, body) => {
-    return cwAPI.put(`${baseUrl}${url}`, body)
+  return cwAPI.put(`${baseUrl}${url}`, body)
 }
 
 export const deleteRequest = (url, body) => {
-    return cwAPI.delete(`${baseUrl}${url}`, body)
+  return cwAPI.delete(`${baseUrl}${url}`, body)
 }
 
 export const patchRequest = (url, body) => {
-    return cwAPI.patch(`${baseUrl}${url}`, body)
+  return cwAPI.patch(`${baseUrl}${url}`, body)
 }
 
 //extract X_tan id
 export function getDynamicTenantId() {
-    const currentURL = window.location.href;
-    const urlParts = currentURL.split('.');
-    if (urlParts[0].includes('https')) {
-        return urlParts[0].replace('https://', '');
-    }
-    return urlParts[0].replace('http://', '');
+  const currentURL = window.location.href;
+  const urlParts = currentURL.split('.');
+  if (urlParts[0].includes('https')) {
+    return urlParts[0].replace('https://', '')
+  }
+  return urlParts[0].replace('http://', '')
 }
 
 export default cwAPI
