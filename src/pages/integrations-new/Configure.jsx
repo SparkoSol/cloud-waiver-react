58071ca8c62109b05a567c40701086f4bccff687
Sparko-Sol <accounts@sparkosol.com>
2024-01-03 17:25:46 +0500

    fixed dropdown issue

--- src/pages/integrations-new/Configure.jsx
+++ src/pages/integrations-new/Configure.jsx
@@ -1,65 +1,84 @@
 import React, {useEffect, useState} from "react";
-
-import DataTable from "../../components/DataTable";
 import Button from "../../components/Button";
 import SocialServiceCard from "./components/SocialServiceCard";
 import SocialServiceRow from "./components/SocialServiceRow";
 import {data} from "./const/services_data";
-import {useLocation, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {selectAllWaivers} from "../../redux/waivers/waiverSlice";
 import {getAllWaiver} from "../../redux/waivers/waiverThunk";
 import {selectIntegrations} from "../../redux/integration-new/integrationSlice";
 import {getRequest, patchRequest} from "../../redux/cwAPI";
 import {toast} from 'react-hot-toast';
-import Spinner from "../../components/Spinner";
 
 const Configure = () => {
-  const {id} = useParams();
-  const dispatch = useDispatch();
-  const item = data.find((item) => item.id === id)
-  const allWaivers = useSelector(selectAllWaivers)
-  const integrationStatus = useSelector(selectIntegrations)
-  const [loading, setLoading] = useState(false);
-  const [folders, setFolders] = useState([]);
+    const {id} = useParams();
+    const dispatch = useDispatch();
+    const item = data.find((item) => item.id === id)
+    const allWaivers = useSelector(selectAllWaivers)
+    const integrationStatus = useSelector(selectIntegrations)
+    const [loading, setLoading] = useState(false);
+    const [folders, setFolders] = useState([]);
 
-  useEffect(() => {
-    dispatch(getAllWaiver());
-    getRequest(`/integration/${item.id}/folders`)
-      .then(r => setFolders(r.data.map(item => item.name)))
-      .catch(e => toast.error(e.response.data.message))
-      .finally(() => setLoading(false))
-  }, [dispatch, item]);
 
-  const handleSubmit = async (id) => {
-    const folders = allWaivers.reduce((acc, item) => {
-      if (item.hasOwnProperty('folder_name')) {
-        acc.push({ folder_name: item.folder, waiver_id: item._id });
-      }
-      return acc;
-    }, []);
-    patchRequest(`/integration/${id}`, {
-      authenticated: true,
-      waiver_folders: folders
-    });
-  }
-  return (
-    <>
-      <SocialServiceCard
-        item={item}
-        state={integrationStatus[item.id]}
-        showConfig={true}
-        width={'w-[500px]'}
-      />
-      <DataTable colspan={0} headers={["Template Name", "Choose Folder"]}
-                 TableRow={SocialServiceRow}
-                 items={allWaivers || []}
-                 folders={folders}
-      />
-      <Button btnClasses='bg-btnBg mt-4 ml-auto' onClick={() => handleSubmit(id)} btnText='Save Changes'></Button>
-      {loading && <Spinner/>}
-    </>
-  );
+    useEffect(() => {
+        dispatch(getAllWaiver());
+        setLoading(true)
+        getRequest(`/integration/${item.id}/folders`)
+            .then(r => setFolders(r.data.map(item => item.name)))
+            .catch(e => toast.error(e.response.data.message))
+            .finally(() => setLoading(false))
+    }, [dispatch, item]);
+
+    const handleSubmit = async (id) => {
+        const folders = allWaivers.reduce((acc, item) => {
+            if (item.hasOwnProperty('folder_name')) {
+                acc.push({folder_name: item.folder, waiver_id: item._id});
+            }
+            return acc;
+        }, []);
+        patchRequest(`/integration/${id}`, {
+            authenticated: true,
+            waiver_folders: folders
+        });
+    }
+    return (
+        <>
+            <SocialServiceCard
+                item={item}
+                state={integrationStatus[item.id]}
+                showConfig={true}
+                width={'w-[500px]'}
+            />
+
+            {(allWaivers && allWaivers.length > 0) ? <div>
+                    <table className={'w-full mt-2 border'}>
+                        <thead className='border h-12 text-sm text-left font-semibold text-gray-600 bg-gray-50'>
+                        <tr>
+                            <th className={'px-4'}>Template Name</th>
+                            <th className={'px-4'}>Choose Folder</th>
+                        </tr>
+                        </thead>
+                        <tbody className={'bg-white'}>
+                        {
+                            allWaivers?.map((item, index) => <SocialServiceRow key={index} item={item}
+                                                                               folders={folders}
+                                                                               index={index}/>)
+                        }
+                        </tbody>
+                    </table>
+                    <Button btnClasses='bg-btnBg mt-4 ml-auto' onClick={() => handleSubmit(id)}
+                            btnText='Save Changes'></Button>
+                </div> :
+                <div className={'flex justify-center mt-28'}>
+                    <div
+                        className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-sky-500 border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+            }
+        </>
+    );
 };
 
 export default Configure;
--- src/pages/integrations-new/components/SocialServiceRow.jsx
+++ src/pages/integrations-new/components/SocialServiceRow.jsx
@@ -4,27 +4,23 @@ import {useDispatch} from "react-redux";
 import {updateFolder} from "../../../redux/waivers/waiverSlice";
 
 const SocialServiceRow = ({item, folders, index}) => {
-  const dispatch = useDispatch();
-  const [selectedFolder, setSelectedFolder] = useState(null)
+    const dispatch = useDispatch();
+    const [selectedFolder, setSelectedFolder] = useState(null)
 
-  function handleChange(folder) {
-    setSelectedFolder(folder)
-    dispatch(updateFolder({index, folder}))
-  }
+    function handleChange(folder) {
+        setSelectedFolder(folder)
+        dispatch(updateFolder({index, folder}))
+    }
 
-  return (
-    <>
-      <tr>
-        <td className='py-4 px-6 text-sm text-gray-900 whitespace-nowrap'>{item.name}</td>
-        <td className='py-4 px-6 text-sm text-gray-900 whitespace-nowrap'>
-          <div>
-            <SelectInput extraClasses='grow md:grow-0' options={folders} setState={handleChange}
-                         state={selectedFolder}/>
-          </div>
-        </td>
-      </tr>
-    </>
-  );
+    return (
+        <tr>
+            <td className='py-4 px-6 text-sm text-gray-900 whitespace-nowrap'>{item.name}</td>
+            <td className='py-4 px-6 text-sm text-gray-900 whitespace-nowrap'>
+                <SelectInput extraClasses='grow md:grow-0' options={folders} setState={handleChange}
+                             state={selectedFolder}/>
+            </td>
+        </tr>
+    );
 };
 
 export default SocialServiceRow;
