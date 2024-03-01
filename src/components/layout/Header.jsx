2abba694a079059fa254116ca875f58acf244a23
Sufyan Zaki <accounts@sparkosol.com>
2024-03-01 11:06:46 +0500

    Added logo in header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,7 +5,7 @@ import {Cog6ToothIcon} from "@heroicons/react/24/outline";
 import {useEffect, useState} from "react";
 import {selectCurrentUser} from "../../redux/user/userSlice";
 import {useSelector} from "react-redux";
-import {useLocation} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import ConfirmationModal from "../modals/ConfirmationModal";
 import {persistor} from "../../redux/store";
 
@@ -97,6 +97,9 @@ const Header = ({ setOpen }) => {
           onClick={() => setOpen((prev) => !prev)}
           iconClasses="w-4 h-4 text-white"
         />
+        <Link className={`transition-all duration-500 lg:hidden`} to={'/dashboard'}>
+          <img className='w-full max-w-[70px] mx-auto' alt='Loading..' src='/images/logo.png'/>
+        </Link>
       </div>
       <div className="flex items-center gap-8 px-4 py-2.5 hidden lg:flex">
         <abbr title="Settings">
